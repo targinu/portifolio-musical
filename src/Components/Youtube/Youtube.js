b204ecbe3a18a0970dbca491f22db660c14f1988
targinu <targino26@outlook.com>
2024-03-07 10:21:25 -0300

    update footer + buttons

--- src/Components/Youtube/Youtube.js
+++ src/Components/Youtube/Youtube.js
@@ -22,7 +22,6 @@ function Youtube() {
               </Icon>
               <span>
                 <a
-                  style={{ color: "black" }}
                   href="https://www.youtube.com/channel/UCRWRFsuyHeex9TcwK3us49A?sub_confirmation=1"
                   target="_blank"
                   rel="noopener noreferrer"
